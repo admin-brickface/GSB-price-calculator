74562473ad66caec11f0341f97fb0386e98cd02d
admin-brickface <admin@brickface.com>
2025-10-01 19:54:27 -0400

    Update App.js
    adjust scaling of pdf

--- src/App.js
+++ src/App.js
@@ -2702,7 +2702,7 @@ function App() {
       margin: 10,
       filename: fileName,
       image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2, useCORS: true },
+      html2canvas: { scale: 1, useCORS: true },
       jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
     };
 
