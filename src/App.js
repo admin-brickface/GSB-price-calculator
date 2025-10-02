3965faa9738ddd9b17863b74eea2fbe2f785e63d
admin-brickface <admin@brickface.com>
2025-10-01 20:03:09 -0400

    Update App.js
    further scaling adjustment

--- src/App.js
+++ src/App.js
@@ -2699,12 +2699,13 @@ function App() {
     const fileName = `${activeTab}_${customerInfo.name || 'estimate'}_${timestamp}.pdf`;
 
     const options = {
-      margin: 10,
+      margin: 5,
       filename: fileName,
       image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 1, useCORS: true },
+      html2canvas: { scale: 0.75, useCORS: true },
       jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
     };
+
 
     html2pdf().set(options).from(element).save();
   };
